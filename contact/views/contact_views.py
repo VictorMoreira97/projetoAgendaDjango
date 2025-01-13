20e3c1c05df9f0d45caa2e1ad4983a27cb22ba70
Victor Chaves Moreira <101597103+VictorMoreira97@users.noreply.github.com>
2025-01-13 09:54:45 -0300

    m

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,31 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
-
 from contact.models import Contact
 
 
 def index(request):
+    search_value = request.GET.get('q', '').strip()
+    
+    if search_value == '':
+        return redirect('contact:index')
+
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[10:20]
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value) 
+        )\
+        .order_by('-id')
+
+    paginator = Paginator(contact, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'contacts': page_obj,
         'site_title': 'Contatos - '
     }
 
