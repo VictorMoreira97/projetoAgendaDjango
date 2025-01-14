693530d7931ef321df87c247203a477d743f9090
Victor Chaves Moreira <101597103+VictorMoreira97@users.noreply.github.com>
2025-01-14 07:41:14 -0300

    m

--- contact/urls.py
+++ contact/urls.py
@@ -6,9 +6,10 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('', views.index, name='index'),  
-    path('search/', views.contact, name='contact'), 
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
 
-    #contact (CRUD)
-    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
 ]
